60291b823b3f816e8260292ef1128055f838782e
Julio Avila <javila1997@gmail.com>
2018-10-03 23:47:01 -0400

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,8 +7,49 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @Input ("sequence") sequence; //Obtenemos el valor escrito en el input
+  sequenceList: Array <any> = []; //Arreglo de la secuencia de numeros
+  continue : boolean = true; //Bool para determinar si continuamos
 
+  constructor(public navCtrl: NavController) {
   }
 
+  //Metodo para observar que tipo de numero es
+  checkSequence(num : number){
+    while(this.continue == true){
+      if (num == 1){ //Si el numero = 1 la conjetura esta completa
+        this.continue = false;
+        console.log("Collatz Conjeture Complete!");
+        break;
+      }
+      if (num % 1 === 0){ //Comprobamos si el numero es entero
+        (num%2)?this.oddNumber(num):this.evenNumber(num); //Ternario para determinar si es par o impar
+      }
+      if (num % 1 !== 0){ //Comprabamos si el numero es decimal
+        this.continue = false;
+        break;
+      }
+    }
+  }
+
+  //Metodo para aplicar la conjetura si el numero es par
+  evenNumber(num : number) {
+    let op = num/2;
+    this.sequenceList.push(op);
+    this.checkSequence(op);
+  }
+
+  //Metodo para aplicar la conjetura si el numero es impar
+  oddNumber(num : number) {
+    let op = (num*3)+1;
+    this.sequenceList.push(op);
+    this.checkSequence(op);
+  }
+
+  //Metodo para limpiar el arreglo y el input permitiendo volver aplicar la conjetura con otro numero
+  reset() {
+    this.sequenceList = [];
+    this.continue = true;
+    this.sequence = " ";
+  }
 }
